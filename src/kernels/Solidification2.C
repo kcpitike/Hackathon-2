3618f51d48eb354dc8d53360feaa5f11315baf11
Krishna Pitike <krishna@Krishnas-MacBook-Pro.local>
2016-05-03 12:34:29 -0500

    updated shit

--- src/kernels/Solidification2.C
+++ src/kernels/Solidification2.C
@@ -23,6 +23,6 @@ Solidification2::computeQpResidual()
 {
   Real DimLessFD1 = 0.0;
   Real Lamda = 15.9566;
-  DimLessFD1 += (((-1) * 15.9566 * _U[_qp] * std::pow(_Phi[_qp], 4.0)) + (2 * 15.9566 * _U[_qp] * std::pow(_Phi[_qp], 2.0)) + (_Phi[_qp]) + ((-1) * 15.9566 * _U[_qp])) * _test[_i][_qp];
+  DimLessFD1 += (((-1) * 15.9566 * _U[_qp] * std::pow(_Phi[_qp], 4.0)) + (2 * 15.9566 * _U[_qp] * std::pow(_Phi[_qp], 2.0)) + (_Phi[_qp]) + ((-1) * 15.9566 * _U[_qp])) * _test[_i][_qp] / 2;
   return DimLessFD1;
 }
